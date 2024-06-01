6ae8f6504c3b9d6f266f10497533198bd0733dfa
PatrikB <bekpatrik24@gmail.com>
2024-06-01 09:03:57 +0200

    void feladatok()

--- src/gyakorlasminta/GyakorlasMinta.java
+++ src/gyakorlasminta/GyakorlasMinta.java
@@ -6,7 +6,12 @@ public class GyakorlasMinta {
    
     public static void main(String[] args) {
         
+       //nem kell komment, de már töröltem így beraktam ezt
+       feladatok();
         
+    }
+
+    private static void feladatok() {
         
     }
     
